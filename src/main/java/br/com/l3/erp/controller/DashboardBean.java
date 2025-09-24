48b2213175acc014520594b4a378e66658772fc8
Lucas <lucasjsm2001@gmail.com>
2025-09-24 01:10:12 -0300

    feat: graficos de vendas

--- src/main/java/br/com/l3/erp/controller/DashboardBean.java
+++ src/main/java/br/com/l3/erp/controller/DashboardBean.java
@@ -1,4 +1,4 @@
-package br.com.l3.erp.controller; 
+package br.com.l3.erp.controller;
 
 import java.io.Serializable;
 import java.util.List;
@@ -8,10 +8,17 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.line.LineChartDataSet;
+import org.primefaces.model.charts.line.LineChartModel;
+import org.primefaces.model.charts.pie.PieChartDataSet;
+import org.primefaces.model.charts.pie.PieChartModel;
+
 import br.com.l3.erp.model.dao.estoque.EstoqueDAO;
 import br.com.l3.erp.model.dao.fornecedor.FornecedorDAO;
 import br.com.l3.erp.model.dao.produto.ProdutoDAO;
 import br.com.l3.erp.model.dao.usuario.UsuarioDAO;
+import br.com.l3.erp.model.dao.venda.VendaDAO;
 import br.com.l3.erp.model.entity.estoque.Estoque;
 
 @Named
@@ -25,23 +32,31 @@ public class DashboardBean implements Serializable {
     private long totalFornecedores;
 
     private List<Estoque> produtosComEstoqueBaixo;
-    
+
+    private LineChartModel vendasPorMesModel;
+    private PieChartModel vendasPorPagamentoModel;
+
     @Inject
     private UsuarioDAO usuarioDAO;
 
-     @Inject
-     private ProdutoDAO produtoDAO;
+    @Inject
+    private ProdutoDAO produtoDAO;
 
-     @Inject
-     private FornecedorDAO fornecedorDAO;
-     
-     @Inject
-     private EstoqueDAO estoqueDAO;
+    @Inject
+    private FornecedorDAO fornecedorDAO;
+
+    @Inject
+    private EstoqueDAO estoqueDAO;
+
+    @Inject
+    private VendaDAO vendaDAO;
 
     @PostConstruct
     public void init() {
         carregarContadores();
         verificarEstoqueBaixo();
+        criarGraficoVendasPorMes();
+        criarGraficoVendasPorFormaPagamento();
     }
 
     private void carregarContadores() {
@@ -54,10 +69,76 @@ public class DashboardBean implements Serializable {
         this.produtosComEstoqueBaixo = estoqueDAO.buscarProdutosComEstoqueBaixo();
     }
 
-    public boolean isAlertaEstoqueVisivel() {
-        return this.produtosComEstoqueBaixo != null && !this.produtosComEstoqueBaixo.isEmpty();
+    // --- Gráfico de vendas por mês ---
+    private void criarGraficoVendasPorMes() {
+        vendasPorMesModel = new LineChartModel();
+        ChartData data = new ChartData();
+
+        LineChartDataSet dataSet = new LineChartDataSet();
+        dataSet.setLabel("Vendas por Mês");
+        dataSet.setFill(false);
+        dataSet.setBorderColor("rgb(75, 192, 192)");
+        dataSet.setTension(0.1);
+
+        List<Object[]> resultados = vendaDAO.vendasPorMes();
+        List<Object> valores = new java.util.ArrayList<>();
+        List<String> labels = new java.util.ArrayList<>();
+
+        for (Object[] linha : resultados) {
+            Integer mes = (Integer) linha[0];
+            Long total = (Long) linha[1];
+            labels.add("Mês " + mes);
+            valores.add(total);
+        }
+        
+        labels.add("Janeiro");
+        labels.add("Fevereiro");
+        labels.add("Março");
+        labels.add("Abril");
+        labels.add("Maio");
+        labels.add("Junho");
+        labels.add("Julho");
+        labels.add("Agosto");
+        labels.add("Setembro");
+
+        dataSet.setData(valores);
+        data.addChartDataSet(dataSet);
+        data.setLabels(labels);
+
+        vendasPorMesModel.setData(data);
     }
     
+ // --- Gráfico de vendas por forma de pagamento ---
+    private void criarGraficoVendasPorFormaPagamento() {
+        vendasPorPagamentoModel = new PieChartModel();
+        ChartData data = new ChartData();
+
+        PieChartDataSet dataSet = new PieChartDataSet();
+        List<Number> valores = new java.util.ArrayList<>();
+        List<String> labels = new java.util.ArrayList<>();
+        List<String> cores = List.of("#42A5F5", "#66BB6A", "#FFA726", "#AB47BC", "#26A69A");
+
+        List<Object[]> resultados = vendaDAO.vendasPorFormaPagamento();
+
+        int corIndex = 0;
+        for (Object[] linha : resultados) {
+            String forma = (linha[0] != null) ? linha[0].toString() : "Não Informado";
+            Long total = (Long) linha[1];
+            labels.add(forma);
+            valores.add(total);
+            corIndex++;
+        }
+
+        dataSet.setData(valores);
+        dataSet.setBackgroundColor(cores);
+        data.addChartDataSet(dataSet);
+        data.setLabels(labels);
+
+        vendasPorPagamentoModel.setData(data);
+    }
+   
+
+    // Getters
     public long getTotalUsuarios() {
         return totalUsuarios;
     }
@@ -69,8 +150,20 @@ public class DashboardBean implements Serializable {
     public long getTotalFornecedores() {
         return totalFornecedores;
     }
-    
+
     public List<Estoque> getProdutosComEstoqueBaixo() {
         return produtosComEstoqueBaixo;
+    }
+
+    public LineChartModel getVendasPorMesModel() {
+        return vendasPorMesModel;
+    }
+
+    public PieChartModel getVendasPorPagamentoModel() {
+        return vendasPorPagamentoModel;
+    }
+
+    public boolean isAlertaEstoqueVisivel() {
+        return this.produtosComEstoqueBaixo != null && !this.produtosComEstoqueBaixo.isEmpty();
     }
 }
--- src/main/java/br/com/l3/erp/model/dao/venda/VendaDAO.java
+++ src/main/java/br/com/l3/erp/model/dao/venda/VendaDAO.java
@@ -1,48 +1,80 @@
 package br.com.l3.erp.model.dao.venda;
 
 import br.com.l3.erp.model.entity.venda.Venda;
-import java.io.Serializable;
-import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.List;
 
 @ApplicationScoped
-@Transactional
 public class VendaDAO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("erpPU");
 
-    
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
     public Venda salvar(Venda venda) {
-        EntityManager em = emf.createEntityManager();
-        if (venda.getIdVenda() == null) {
-        	em.getTransaction().begin();
-            em.persist(venda);
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            if (venda.getIdVenda() == null) {
+                em.persist(venda);
+            } else {
+                venda = em.merge(venda);
+            }
             em.getTransaction().commit();
-            return venda; // Retorna a instância com o ID gerado
-        } else {
-            return em.merge(venda);
+            return venda;
+        } finally {
+            em.close();
         }
     }
-    
+
     public List<Venda> buscarTodas() {
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = getEntityManager();
         try {
             return em.createQuery("FROM Venda", Venda.class).getResultList();
         } finally {
             em.close();
         }
     }
-    
-    public List<Venda> listarTodos() {
-        EntityManager em = emf.createEntityManager();
 
-        return em.createQuery("SELECT v FROM Venda v", Venda.class).getResultList();
+    /**
+     * Retorna quantidade de vendas agrupadas por mês (1 a 12).
+     */
+    public List<Object[]> vendasPorMes() {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery(
+                    "SELECT MONTH(v.dataVenda), COUNT(v) " +
+                    "FROM Venda v " +
+                    "GROUP BY MONTH(v.dataVenda) " +
+                    "ORDER BY MONTH(v.dataVenda)", Object[].class
+            ).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    /**
+     * Retorna quantidade de vendas agrupadas por forma de pagamento.
+     */
+    public List<Object[]> vendasPorFormaPagamento() {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery(
+                    "SELECT v.formaPagamento, COUNT(v) " +
+                    "FROM Venda v " +
+                    "GROUP BY v.formaPagamento", Object[].class
+            ).getResultList();
+        } finally {
+            em.close();
+        }
     }
 }
